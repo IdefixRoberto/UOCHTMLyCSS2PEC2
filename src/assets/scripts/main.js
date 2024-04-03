2cec0ba5ae241bf7c202a12e2a2c89d186277dd5
IdefixRoberto <peipal@uoc.edu>
2024-04-03 22:45:40 +0200

    Pujar Practica

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -19,69 +19,113 @@
 const nomInput = document.getElementById("nom");
 const telefonInput = document.getElementById("telefon");
 const emailInput = document.getElementById("email");
-const submitButton = document.querySelector("button");
+const submitButton = document.getElementById("submit-btn");
+const errorNom = document.getElementById("error-nom");
+const errorTelefon = document.getElementById("error-telefon");
+const errorEmail = document.getElementById("error-email");
+const amagarFormulari = document.getElementById("amagar");
+const homeFormulari = document.getElementById("home-g");
+const missatgeEnviat = document.getElementById("form-Wiew")
 
-const validarNom = (nombr) => {
-  if (nombre.length < 5) {
-    return false;
-  }
-  return true;
+
+
+const validarNom = (nombre) => {
+    return nombre.length >= 5;
 };
 
 const validarTelefon = (telefon) => {
-  const regex = /^\d{9}$/;
-  return regex.test(telefon);
+    const regex = /^\d{9}$/;
+    return regex.test(telefon);
 };
 
 const validarEmail = (email) => {
-  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.test(email);
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+};
+  
+
+const mostrarError = (input, mensaje, corregir) => {
+    input.textContent = mensaje;
+    input.classList.add("error");
+    corregir.classList.add("errorInput");
 };
 
-const mostrarError = (input, mensaje) => {
-  const errorElement = input.parentNode.querySelector(".error-" + input.id);
-  errorElement.textContent = mensaje;
-  input.classList.add("error");
-};
-
-const ocultarError = (input) => {
-  const errorElement = input.parentNode.querySelector(".error-" + input.id);
-  errorElement.textContent = "";
-  input.classList.remove("error");
+const ocultarError = (input, corregir) => {
+    input.textContent = "";
+    input.classList.remove("error");
+    corregir.classList.remove("errorInput");
 };
 
 const actualizarBoton = () => {
-  if (nombreInput.value && telefonInput.value && emailInput.value) {
-    submitButton.disabled = false;
-  } else {
-    submitButton.disabled = true;
-  }
+    submitButton.disabled = !(validarNom(nomInput.value) && validarTelefon(telefonInput.value) && validarEmail(emailInput.value));
 };
 
-nombreInput.addEventListener("input", () => {
-  if (validarNom(nomInput.value)) {
-    ocultarError(nomInput);
-  } else {
-    mostrarError(nomInput, "El nombre debe tener al menos 5 caracteres");
-  }
-  actualizarBoton();
+
+
+submitButton.addEventListener("click", () => {
+
+    missatgeEnviat.classList.add("opacity");
+    missatgeEnviat.classList.remove("Noopacity");
+    setTimeout(function() {
+        missatgeEnviat.classList.add("Noopacity");
+        missatgeEnviat.classList.remove("opacity");
+      }, 5000);
+
 });
 
-telefonoInput.addEventListener("input", () => {
-  if (validarTelefono(telefonInput.value)) {
-    ocultarError(telefonInput);
-  } else {
-    mostrarError(telefonInput, "El teléfono no tiene un formato válido");
-  }
-  actualizarBoton();
+nomInput.addEventListener("input", () => {
+    actualizarBoton();
+    if (validarNom(nomInput.value)) {
+        ocultarError(errorNom, nomInput);
+    } else {
+        mostrarError(errorNom, "El nom deu contenir com a minim 5 caràcters", nomInput);
+    }
+
+});
+
+telefonInput.addEventListener("input", () => {
+    actualizarBoton();
+    if (validarTelefon(telefonInput.value)) {
+        console.log("Hola")
+        ocultarError(errorTelefon, telefonInput);
+    } else {
+        mostrarError(errorTelefon, "El teléfon no te un format vàlit", telefonInput);
+    }
+
 });
 
 emailInput.addEventListener("input", () => {
-  if (validarEmail(emailInput.value)) {
-    ocultarError(emailInput);
-  } else {
-    mostrarError(emailInput, "El email no tiene un formato válido");
-  }
-  actualizarBoton();
+    actualizarBoton();
+    if (validarEmail(emailInput.value)) {
+        ocultarError(errorEmail, emailInput);
+        
+    } else {
+        mostrarError(errorEmail, "El email no te un format vàlit", emailInput);
+    }
+    
 });
 
+
+function amagar() { 
+
+
+    if(amagarFormulari.textContent=="Subscriures"){
+        homeFormulari.classList.remove("dissableForm")
+    amagarFormulari.textContent = "Amagar Formulari";
+
+    amagarFormulari.classList.remove("amagar");
+    amagarFormulari.classList.add("amagar2");
+        }
+
+        else{
+            amagarFormulari.textContent = "Subscriures";
+            homeFormulari.classList.add("dissableForm");
+            amagarFormulari.classList.remove("amagar");
+            amagarFormulari.classList.add("amagar2")
+        }
+
+}
+
+amagarFormulari.addEventListener("click", () =>{
+    amagar()
+})
